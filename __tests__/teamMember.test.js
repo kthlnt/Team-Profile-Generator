d7daa5ce544f7f14230e333ba572ab59efb88523
Katherine Lantto <kathlantto@gmail.com>
2023-01-24 19:19:13 -0700

    adding test team members to js files

---
+++ __tests__/teamMember.test.js
@@ -0,0 +1,30 @@
+const teamMember = require('../lib/teamMember');
+
+describe('Team Member', () => {
+    describe('Initialization', () => {
+        it("should create a new team member instance with name, title, and email", () => {
+            const name = "Kat";
+            const title = "Project Manager";
+            const email = "example@gmail.com";
+
+            const newTeamMember = new teamMember(name, title, email);
+
+            expect(newTeamMember instanceof teamMember).toBe(true);
+            expect(newTeamMember.name).toEqual("Kat");
+            expect(newTeamMember.title).toEqual("Project Manager");
+            expect(newTeamMember.email).toEqual("example@gmail.com");
+        });
+    });
+    describe('getTitle', ()=> {
+        it("should return the title of the member", () => {
+            const name = "Kat";
+            const title = "Project Manager";
+            const email = "example@gmail.com";
+            const newTeamMember = new teamMember(name, title, email);
+
+            const result = newTeamMember.getTitle();
+
+            expect(result).toEqual("example@gmail.com");
+        });
+    });
+});
