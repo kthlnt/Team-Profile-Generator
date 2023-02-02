bf600cf5765f1efff86c7f515efc1b08d9d87e40
Katherine Lantto <kathlantto@gmail.com>
2023-02-02 14:22:30 -0700

    renamed teammembers as it was being used for two different things causing errors in code

--- index.js
+++ index.js
@@ -1,155 +1,153 @@
-const inq = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-const Manager = require("./lib/Manager");
-const TeamMember = require("./lib/NonManager");
-const renderTeam = require("./src/createTeam");
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/intern');
+const organizeTeam = require('./src/createTeam');
 
 const DIST_DIR = path.resolve(__dirname, "dist");
 const distPath = path.join(DIST_DIR, "index.html");
 
 const teamMembers = [];
+const addToTeam = [];
 
+const questions = () => {
 
-console.log(
-    "\nWelcome to the team profile generator!\nUse `npm run reset` to reset the dist/ folder\n"
-);
-
-const appMenu = () => {
-    function createTeam() {
-        inq
+    const createTeam = () => {
+        inquirer
             .prompt([
                 {
                     type: "list",
-                    name: "teamMemberChoice",
-                    message: "Which type of team member would you like to add?",
+                    name: "teamMember",
+                    message: "Which type of employee would you like to add?",
                     choices: [
                         "Manager",
-                        "Non-manager",
-                        "I don't need to add any more members."
+                        "Engineer",
+                        "Intern",
+                        "I don't need any more employees."
                     ]
                 }
             ])
             .then((response) => {
-                switch (response.teamMemberChoice) {
+                switch (response.teamMember) {
                     case "Manager":
-                        addManager();
+                        createManager();
                         break;
-                    case "Non-manager":
-                        addNonManager();
+                    case "Engineer":
+                        createEngineer();
+                        break;
+                    case "Intern":
+                        createIntern();
                         break;
                     default:
                         buildTeam();
                 }
             });
-    }
 
-    function addManager() {
-        inq
+    }
+    const createManager = () => {
+        inquirer
             .prompt([
                 {
                     type: "input",
                     name: "managerName",
-                    validate: (answer) => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "You have to enter the manager name.";
-                    }
-                },
-                {
-                    type: "input",
-                    name: "managerTitle",
-                    message: "What is the manager's job title?",
-                    validate: (answer) => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "You have to enter a job title.";
-                    }
+                    message: "Manager's Full Name:",
                 },
                 {
                     type: "input",
                     name: "managerEmail",
-                    message: "What is the manager's email?",
-                    validate: (answer) => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "You must enter an email.";
-                    }               
-                }
+                    message: "Manager's Email:",
+                },
+                {
+                    type: "input",
+                    name: "managerOfficeNum",
+                    message: "Manager's Office Number:",
+                },
             ])
             .then((response) => {
                 const manager = new Manager(
                     response.managerName,
-                    response.managerTitle,
-                    response.managerEmail
+                    response.managerEmail,
+                    response.managerOfficeNum
                 );
                 teamMembers.push(manager);
+                addToTeam.push(response.managerID);
                 createTeam();
             });
     }
 
-    function addNonManager() {
-        inq
+    const createEngineer = () => {
+        inquirer
             .prompt([
                 {
                     type: "input",
-                    name: "nonManagerName",
-                    message: "What is your team member's name?",
-                    validate: (answer) => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "You have to enter a name.";
-                    }
+                    name: "engineerName",
+                    message: "Engineer's Full Name:",
                 },
                 {
                     type: "input",
-                    name: "nonManagerTitle",
-                    message: "What is your team member's job title?",
-                    validate: (answer) => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "You have to enter a job title.";
-                    }
+                    name: "engineerEmail",
+                    message: "Engineer's Email:",
                 },
                 {
                     type: "input",
-                    name: "nonManagerEmail",
-                    message: "What is your team member's email?",
-                    validate: (answer) => {
-                        if (answer !== "") {
-                            return true;
-                        }
-                        return "You have to enter an email.";
-                    }
+                    name: "engineerGitHub",
+                    message: "GitHub Username:"
                 },
-
             ])
             .then((response) => {
-                const teamMember = new TeamMember(
-                    response.nonManagerName,
-                    response.nonManagerTitle,
-                    response.nonManagerEmail
+                const engineer = new Engineer(
+                    response.engineerName,
+                    response.engineerEmail,
+                    response.engineerGitHub
                 );
-                teamMembers.push(teamMember);
+                teamMembers.push(engineer);
+                addToTeam.push(response.engineerId);
                 createTeam();
             });
+    }
+
+    const createIntern = () => {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "internName",
+                    message: "Intern's Full Name:",
+                },
+                {
+                    type: "input",
+                    name: "internEmail",
+                    message: "Intern's Email:",
+                },
+                {
+                    type: "input",
+                    name: "internSchool",
+                    message: "Intern's College, University, or Program:",
+                },
+            ])
+        .then((response) => {
+            const intern = new Intern(
+                response.internName,
+                response.internEmail,
+                response.internSchool
+            );
+            teamMembers.push(intern);
+            addToTeam.push(response.internId);
+            createTeam();
+        });
     }
 
     const buildTeam = () => {
         if (!fs.existsSync(DIST_DIR)) {
             fs.mkdirSync(DIST_DIR);
         }
-        fs.writeFileSync(distPath, renderTeam(teamMembers), "utf-8");
+        fs.writeFileSync(distPath, organizeTeam(teamMembers), "utf-8");
     };
 
     createTeam();
+
 };
 
-
-
-appMenu();
+questions();
--- src/createTeam.js
+++ src/createTeam.js
@@ -1,91 +1,107 @@
-const teamMember = require("../lib/TeamMember");
-
 const createTeam = (team) => {
-    const addManager = (manager) => {
+    const createManager = (manager) => {
         return `
-        <div class="col d-flex justify-content-center">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item">${manager.getName()}</li>
-              <li class="list-group-item">${manager.getTitle()}</li>
-              <li class="list-group-item">A third item</li>
-            </ul>
-            <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-    </div>
-  `
+        <div class=" col d-flex justify-content-center">
+            <div class="card" style="width: 18rem">
+                <div class="card-body">
+                    <ul class="list-group">
+                    <i class="fa-solid fa-people-roof"></i>
+                    <li class="list-group-item heading">${manager.getName()}</li>
+                    <li class="list-group-item">${manager.getRole()}</li> 
+                    <a href="mailto:${manager.getEmail()}" class="list-group-item">Email: ${manager.getEmail()}</a>
+                    <a href="tel:${manager.getOfficeNumber()}" class="list-group-item">Office #: ${manager.getOfficeNumber()}</a>
+                                        </ul>
+                </div>
+            </div>  
+        </div>      
+        `;
+    };
+    const createEngineer = (engineer) => {
+        return `
+        <div class=" col d-flex justify-content-center">
+            <div class="card" style="width: 18rem">
+                <div class="card-body">
+                    <ul class="list-group">
+                    <i class="fa-solid fa-screwdriver-wrench"></i>
+                    <li class="list-group-item heading">${engineer.getName()}</li>
+                    <li  class="list-group-item">${engineer.getRole()}</li> 
+                    <a href="mailto:${engineer.getEmail()}" class="list-group-item">Email: ${engineer.getEmail()}</a> 
+                    <a href="https://github.com/${engineer.getGitHub()}" class="list-group-item">GitHub: ${engineer.getGitHub()}</a>
+                    </ul>
+                </div>
+            </div>  
+        </div>      
+        `;
     };
 
-    const addNonManager = (member) => {
+    const createIntern = (intern) => {
         return `
-        <div class="col d-flex justify-content-center">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item">${member.getName()}</li>
-            <li class="list-group-item">${member.getRole()}</li>
-            <li class="list-group-item">A third item</li>
-            </ul>
-            <div class="card-body">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-            </div>
-        </div>
-    </div>
-    `
+        <div class=" col d-flex justify-content-center">
+            <div class="card" style="width: 18rem">
+                <div class="card-body">
+                    <ul class="list-group">
+                    <i class="fa-solid fa-mug-hot"></i>
+                    <li class="list-group-item heading">${intern.getName()}</li>
+                    <li  class="list-group-item">${intern.getRole()}</li> 
+                    <a href="mailto:${intern.getEmail()}" class="list-group-item">Email: ${intern.getEmail()}</a> 
+                    <li class="list-group-item">School: ${intern.getSchool()}</li>
+                    </ul>
+                </div>
+            </div>  
+        </div>      
+        `;
     };
-
     const html = [];
 
-        html.push(
-            team
-                .filter((teamMember) => teamMember.getMemberRole() === "Manager")
-                .map((manager) => addManager(manager))
-                .join("")
-        );
-        html.push(
-            team
-                .filter((teamMember) => teamMember.getMemberRole() === "Non-Manager")
-                .map((member) => addNonManager(member))
-                .join("")
-        );
-        return html.join("");
+    html.push(
+        team
+            .filter((teamMember) => teamMember.getRole() === "Manager")
+            .map((manager) => createManager(manager))
+            .join("")
+    );
+    html.push(
+        team
+            .filter((teamMember) => teamMember.getRole() === "Engineer")
+            .map((engineer) => createEngineer(engineer))
+            .join("")
+    );
+    html.push(
+        team
+            .filter((teamMember) => teamMember.getRole() === "Intern")
+            .map((intern) => createIntern(intern))
+            .join("")
+    );
+    return html.join("");
 
 };
 
 module.exports = (team) => {
     return `
     <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
-    crossorigin="anonymous"/>
-    <link href="./styles/css/reset.css" />
-    <link href="./styles/css/style.css"/>
-    <title>Team Profile</title>
-</head>
-<body>
-    <h1 class="p-3 mb-2 bg-primary text-white text-center">Team Profile</h1>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+            <link rel="stylesheet" href="./styles/css/reset.css" />
+            <link rel="stylesheet" href="./styles/css/style.css" />
+            <link rel="shortcut icon" href="../dist/images/logo.JPG">
+        <script src="https://kit.fontawesome.com/f3d669e5cc.js" crossorigin="anonymous"></script>
+        <title>Team Profile Generator</title>
+    </head>
+    
+    <body>
+    <h1>Team Profile Generator</h1>
+    <div class="container text-center">
         <div class="row">
-            ${createTeam(team)}
-        </div> 
+        ${createTeam(team)}
+        </div>
+    </div>
 </body>
 </html>
-`;
-}
+    `;
+
+};
